9d286e2e6aaac2da6783663f09a231cfe95383a2
Nehal Chavan <nehal.chavan@amdocs.com>
2021-01-24 22:47:23 +0530

    Final done

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Nehal Chavan",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Nehal Chavan and I'm a Software Engineer!",
+  githubUrl: "https://github.com/Nehal0007/",
+  linkedinUrl: "https://www.linkedin.com/in/nehal-chavan-2659aa144/",
+  cvLink: "https://drive.google.com/file/d/1JX0zvXiiBsEiJT9l-PCYiIWQo1VYQZ6n/view?usp=sharing/",
+};
